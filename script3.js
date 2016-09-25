9027c453bbc43d715ac1ab28452e864517c87689
Liaquat <liaquatkhan58@gmail.com>
2016-09-25 18:31:48 +0500

    edit script 3 and work on html

---
+++ script3.js
@@ -0,0 +1,68 @@
+//ARRAY 
+// alert("Array");
+
+// var cars = ["mehran","civic","city","corola","parado","surf","doublecabin","bmw","suzuki","honda"];
+// alert(cars[6]);
+
+// var cities = [];
+// cities[0] = "Karachi";
+// cities[1] = "Lahore";
+// cities[2] = "Islamabad";
+// cities[3] = "quetta";
+// cities[4] = "peshawar";
+// cities[5] = "multan";
+// cities.pop();
+// cities.pop();
+// cities.push("sibi","haydrabad")
+// console.log("new");
+
+
+// var number = parseInt(prompt("Enter any number","Here"));
+
+// var a = number * 1 ;
+// var b = number * 2 ;
+// var c = number * 3 ;
+// var d = number * 4 ;
+// var e = number * 5 ;
+// var f = number * 6 ;
+// var g = number * 7 ;
+// var h = number * 8 ;
+// var i = number * 9 ;
+// var j = number * 10 ;
+
+// document.write(number + "*" + "1" + "=" + a);
+// document.write("<br>");
+
+// document.write(number + "*" + "2" + "=" + b);
+// document.write("<br>");
+
+// document.write(number + "*" + "3" + "=" + c);
+// document.write("<br>");
+
+// document.write(number + "*" + "4" + "=" + d);
+// document.write("<br>");
+
+// document.write(number + "*" + "5" + "=" + e);
+// document.write("<br>");
+
+// document.write(number + "*" + "6" + "=" + f);
+// document.write("<br>");
+
+// document.write(number + "*" + "7" + "=" + g);
+// document.write("<br>");
+
+// document.write(number + "*" + "8" + "=" + h);
+// document.write("<br>");
+
+// document.write(number + "*" + "9" + "=" + i);
+// document.write("<br>");
+
+// document.write(number + "*" + "10" + "=" + j);
+// document.write("<br>");
+
+
+var num = parseInt(prompt("Enter you value","Here"));
+if (  )
+
+
+
